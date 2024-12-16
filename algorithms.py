Fixed negative sign bugs

algorithms.py
@@ -42,7 +42,7 @@ def fine_landing_site_selection(site_idx, halss_global, flags, params):
   halss_local.y_cell_size = params.y_cell_size_fine
   
   # Determine the local pointcloud based on the region location
-  halss_local = site_localize(site_idx, halss_global, halss_local)
+  halss_local = region_localize(site_idx, halss_global, halss_local)
 
   # Check if there are enough points in the local pointcloud to generate a surface normal
   if site_check(site_idx, halss_local, flags, params):
@@ -68,7 +68,7 @@ def fine_landing_site_selection(site_idx, halss_global, flags, params):
 
 def update_landing_site(site_idx, halss_global, halss_local, flags, params):
   # Determine the local pointcloud based on the region location
-  halss_local = site_localize(site_idx, halss_global, halss_local)
+  halss_local = region_localize(site_idx, halss_global, halss_local)
   
   # Check if there are enough points in the local pointcloud to generate a surface normal
   if site_check(site_idx, halss_local, flags, params):
@@ -130,10 +130,16 @@ def score_landings(halss_data):
   radius_ned = sf_x * radius
   density_score = np.zeros(len(radius))
   for idx in range(len(radius)):
-    dist = (halss_data.pcd_global[:,0] - halss_data.center_coords_ned[idx][0])**2 + (halss_data.pcd_global[:,1] - (-halss_data.center_coords_ned[idx][1]))**2
+    dist = (halss_data.pcd_global[:,0] - halss_data.center_coords_ned[idx][0])**2 + (halss_data.pcd_global[:,1] - halss_data.center_coords_ned[idx][1])**2
     within = dist < radius_ned[idx]**2
-    density_score[idx] = np.sum(within)/(np.pi*radius_ned[idx]**2)
-  density_score = density_score/density_score.max()
+    if radius_ned[idx] == 0:
+      density_score[idx] = 0
+    else:
+      density_score[idx] = np.sum(within)/(np.pi*radius_ned[idx]**2)
+  if density_score.max() == 0:
+    density_score = np.ones_like(radius)
+  else:
+    density_score = density_score/density_score.max()
 
   # 4. How large is the 99th percentile of the uncertainty map in the landing site
   uncertainty_score = np.ones_like(radius) # deprecated feature (no uncertainty map supported currently)

classes.py
@@ -146,7 +146,7 @@ class halss_data_packet:
     box_x_min, box_y_min = self.ned2uv(0, 0)
     box_x_max, box_y_max = self.ned2uv(x_cell_size/2, y_cell_size/2)
 
-    cell_width = (box_x_max - box_x_min)*scale_factor
+    cell_width  = (box_x_max - box_x_min)*scale_factor
     cell_height = (box_y_max - box_y_min)*scale_factor
     
     for idx in range(len(u_vec)):
@@ -220,8 +220,8 @@ class halss_data_packet:
     # Calculate geometric information about site
     max_x = int(self.safety_map.shape[0]/2 + region_radius)
     min_x = int(self.safety_map.shape[0]/2 - region_radius)
-    max_y = int(self.safety_map.shape[0]/2 + region_radius)
-    min_y = int(self.safety_map.shape[0]/2 - region_radius)
+    max_y = int(self.safety_map.shape[1]/2 + region_radius)
+    min_y = int(self.safety_map.shape[1]/2 - region_radius)
     center_x = (min_x + max_x)/2
     center_y = (min_y + max_y)/2
     off_min_x = 0 if min_x < 0 else min_x
@@ -241,7 +241,10 @@ class halss_data_packet:
       new_radius = region_radius
 
     # Param update
-    radius_sf = new_radius/self.radii_uv[0]
+    if self.radii_uv[0] > 0:
+      radius_sf = new_radius/self.radii_uv[0]
+    else:
+      radius_sf = 0
     self.radii_ned[0] = self.radii_ned[0]*radius_sf
     self.radii_uv[0] = new_radius
     return new_radius
@@ -270,7 +273,7 @@ class halss_data_packet:
     x_scaled = del_x*self.sf_x
     y_scaled = del_y*self.sf_y
     pcd_kd_tree = scipy.spatial.KDTree(self.pcd_global[:,:2])
-    dist,idx_tree = pcd_kd_tree.query(np.array([x_scaled, -y_scaled]))
+    dist,idx_tree = pcd_kd_tree.query(np.array([x_scaled, y_scaled]))
     if dist > 4:
       print("Warning! Your landing site is probably in a interpolated region where no points are present")
     x,y,z = self.pcd_global[idx_tree,:]
@@ -296,7 +299,8 @@ class halss_data_packet:
     # Find NED Origin in UV Pixel Space
     self.scale_uv_2_world()
     x_pcd_to_surface = self.sn_x_min - (self.sn_x_max-self.sn_x_min)/(self.pcd_x_max- self.pcd_x_min) * (self.pcd_x_min)
-    y_pcd_to_surface = self.sn_y_min + (self.sn_y_max-self.sn_y_min) * (0 - self.pcd_y_min)/(self.pcd_y_max - self.pcd_y_min)
+    # y_pcd_to_surface = self.sn_y_min + (self.sn_y_max-self.sn_y_min) * (0 - self.pcd_y_min)/(self.pcd_y_max - self.pcd_y_min)
+    y_pcd_to_surface = self.sn_y_min - (self.sn_y_max-self.sn_y_min)/(self.pcd_y_max- self.pcd_y_min) * (self.pcd_y_min)
   
     self.org_x = x_pcd_to_surface
-    self.org_y = self.sn_y_max - y_pcd_to_surface
+    self.org_y = y_pcd_to_surface